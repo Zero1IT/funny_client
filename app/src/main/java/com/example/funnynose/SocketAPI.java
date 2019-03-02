4fa7c2a155b7b76ce53d8a901b276fd194ce6995
Dima <dima2-8@bk.ru>
2019-03-02 22:03:00 +0300

    .

--- app/src/main/java/com/example/funnynose/SocketAPI.java
+++ app/src/main/java/com/example/funnynose/SocketAPI.java
@@ -9,8 +9,8 @@ import io.socket.client.Socket;
 
 public class SocketAPI {
 
-    //private final static String URL = "http://app.funnynose.by";
-    private final static String URL = "http://192.168.0.105:3000";
+    private final static String URL = "http://app.funnynose.by";
+    //private final static String URL = "http://192.168.0.105:3000";
     //private final static String URL = "http://127.0.0.1:3000";
 
 
